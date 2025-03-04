0b0cb4b4b850238ad9a7f7d1097b5a210155ebfe
zulfikar-ditya <zulfikarditya.work@gmail.com>
2025-03-04 11:03:59 +0700

    ADD: implement Folio

---
+++ resources/views/pages/components.blade.php
@@ -0,0 +1,10 @@
+<x-main>
+    <x-slot name="title">Components</x-slot>
+    <x-slot name="description">This is the components page.</x-slot>
+    <x-slot name="keywords">components, blade, laravel</x-slot>
+    <x-slot name="active">components</x-slot>
+    <x-slot name="body">
+        <h1>Components</h1>
+        <p>This is the components page.</p>
+    </x-slot>
+</x-main>
