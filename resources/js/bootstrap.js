53974fd29229906098011dcc064dd4ba9b440836
zulfikar-ditya <zulfikarditya.work@gmail.com>
2025-03-12 13:28:25 +0700

    add: drag and drop #2

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,17 +1,18 @@
+import "./custom/flatpickr";
+import "./custom/select2";
+import "./custom/draganddrop";
+
 import axios from "axios";
 window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-// Import Alpine.js
 import Alpine from "alpinejs";
 import { mask } from "@alpinejs/mask";
 import { collapse } from "@alpinejs/collapse";
 import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
 import $ from "jquery";
 import select2 from "select2";
-import "select2/dist/css/select2.min.css";
 
 Alpine.plugin(mask);
 Alpine.plugin(collapse);
@@ -25,6 +26,3 @@ window.$ = window.jQuery = $;
 
 select2($);
 window.select2 = select2;
-
-import "./custom/flatpickr";
-import "./custom/select2";
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,8 @@
         </div>
         <div class="card-body">
             <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-                <x-input data-flatpickr="" name="ok" label='date' />
-                <x-input data-flatpickr="year" name="ok" label='date' />
-                <x-input data-flatpickr="time" name="ok" label='date' />
-                <x-select name="name" label="select" class="form-select2 select2" data-select2="">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                </x-select>
+                {{-- <x-input type="file" name="ok" label='date' /> --}}
+                <x-drag-and-drop-input />
             </div>
         </div>
     </div>
