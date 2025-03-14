4c1277c057a83036036ebcfa6ca6608343326cbf
zulfikar-ditya <zulfikarditya.work@gmail.com>
2025-03-14 07:37:23 +0700

    add: card and button radio variant

--- resources/views/components/radio-input.blade.php
+++ resources/views/components/radio-input.blade.php
@@ -1,10 +1,41 @@
 @php
     $name = $attributes->get('name');
+    $style = $attributes->get('style', 'basic');
+
+    $buttonVariant = $attributes->get('variant', 'primary');
+    $buttonVariant = in_array($buttonVariant, ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'dark', 'light']) ? $buttonVariant : 'primary';
+    $buttonVariant = "radio-button-{$buttonVariant}";
 @endphp
-<label class="form-radio" for="label-{{ $attributes->get('id') ?? $attributes->get('name') }}">
-    <input type="radio" {{ $attributes }} />
-    {{ $slot }}
-</label>
-@error($name)
-    <span class="form-error">{{ $message }}</span>
-@enderror
+
+@if ($style === 'basic')
+    <label class="form-radio" for="{{ $attributes->get('id') ?? $attributes->get('name') }}">
+        <input type="radio" {{ $attributes }} />
+        {{ $slot }}
+    </label>
+    @error($name)
+        <span class="form-error">{{ $message }}</span>
+    @enderror
+@endif
+
+@if ($style === 'card')
+    <label for="{{ $attributes->get('id') ?? $name }}" class="radio-card">
+        <div>
+            {{ $slot }}
+        </div>
+
+        <input type="radio" {{ $attributes }} />
+    </label>
+    @error($name)
+        <span class="form-error">{{ $message }}</span>
+    @enderror
+@endif
+
+@if ($style === 'button')
+    <label for="{{ $attributes->get('id') ?? $name }}" class="radio-button {{ $buttonVariant }}">
+        <input type="radio" {{ $attributes }} class="sr-only" />
+        <p class="text-sm font-medium">{{ $slot }}</p>
+    </label>
+    @error($name)
+        <span class="form-error">{{ $message }}</span>
+    @enderror
+@endif
